50b0ac069417fc8189ff9306914dc4dd60d1a126
Cory Tomlinson <corytomlinson6640@gmail.com>
2023-09-12 21:22:01 -0600

    P4 appears to be working

--- src/main/scala/com/github/ptlux1517/leetcode/hard/P4_Median_of_Two_Sorted_Arrays.scala
+++ src/main/scala/com/github/ptlux1517/leetcode/hard/P4_Median_of_Two_Sorted_Arrays.scala
@@ -3,6 +3,8 @@ package com.github.ptlux1517.leetcode.hard
 import com.github.ptlux1517.leetcode.{ColorPrinter, LeetcodeProblem}
 
 import java.time.{Duration, Instant, temporal}, temporal.Temporal
+
+import scala.annotation.tailrec
 import scala.concurrent.duration.FiniteDuration
 
 
@@ -12,17 +14,158 @@ object P4_Median_of_Two_Sorted_Arrays extends LeetcodeProblem {
   val probNum:String = prob.head.drop(1) //drop leading P
   val probName:String = prob.tail.mkString(" ").dropRight(1) //drop trailing $
 
+
+//  private enum Direction {case L, R, None}
+
+//  def findMedianSortedArrays(nums1:Array[Int], nums2:Array[Int]):Double = {
+//    (nums1.length, nums2.length) match {
+//      case (_,0) => median(nums1)
+//      case (0,_) => median(nums2)
+//      case _     => f(
+//        nums1=nums1, low1=0, high1=nums1.length-1, prevDir1 = Direction.None,
+//        nums2=nums2, low2=0, high2=nums2.length-1, prevDir2 = Direction.None
+//      )
+//    }
+//  }
+
+//  private def f(
+//    nums1:Array[Int],low1:Int,high1:Int,prevDir1:Direction,
+//    nums2:Array[Int],low2:Int,high2:Int,prevDir2:Direction
+//  ):Double = {
+//    (nums1(mid(low1,high1)),nums2(mid(low2,high2))) match {
+//      case (e1,e2) if e1 == e2 => {}
+//      case (e1,e2) if e1 <= e2 => {}
+//    }
+//
+//    0.0
+//  }
+
+
   def findMedianSortedArrays(nums1:Array[Int], nums2:Array[Int]):Double = {
-    0.0
+    val combinedLength = nums1.length + nums2.length
+    val singleMedian = combinedLength%2 == 1
+    val stepsToMedian = if singleMedian then combinedLength/2 + 1 else combinedLength/2
+    classifyRelation(nums1,nums2) match {
+      case (_, Relation.Empty)                               => median(nums1)
+      case (Relation.Empty, _)                               => median(nums2)
+      case (Relation.DisjointBefore, Relation.DisjointAfter) => medianDisjoint(numsBefore=nums1, numsAfter=nums2, singleMedian, stepsToMedian)
+      case (Relation.DisjointAfter, Relation.DisjointBefore) => medianDisjoint(numsBefore=nums2, numsAfter=nums1, singleMedian, stepsToMedian)
+      case _                                                 => binaryApproachMedian(
+        nums1, nums2, i1 = -1, i2 = -1, singleMedian, stepsToMedian, stepsTaken=0
+      )
+    }
   }
+
+  @tailrec
+  private def binaryApproachMedian(
+    nums1:Array[Int], nums2:Array[Int], i1:Int, i2:Int,
+    singleMedian:Boolean, stepsToMedian:Int, stepsTaken:Int
+  ):Double = {
+    val remainingSteps = stepsToMedian - stepsTaken
+    if remainingSteps == 0 then {
+      val greaterElem = (i1,i2) match {
+        case (_,-1) => nums1(i1)
+        case (-1,_) => nums2(i2)
+        case _      => nums1(i1) max nums2(i2)
+      }
+      if singleMedian then {
+        return greaterElem
+      }
+      else {
+        val nextGreatestElem = (nums1.length,nums2.length) match {
+          case (l1,l2) if i1+1 < l1 && i2+1 < l2 => nums1(i1+1) min nums2(i2+1)
+          case (l1,l2) if i1+1 < l1 => nums1(i1+1)
+          case (l1,l2) if i2+1 < l2 => nums2(i2+1)
+        }
+        return (greaterElem + nextGreatestElem).toDouble / 2
+      }
+    }
+    else {
+      val binaryJump = if remainingSteps==1 then 1 else remainingSteps / 2
+      val jumpIdx1 = i1+binaryJump min nums1.length-1
+      val jumpIdx2 = i2+binaryJump min nums2.length-1
+      val jumpElem1 = nums1(jumpIdx1)
+      val jumpElem2 = nums2(jumpIdx2)
+      if jumpElem1 <= jumpElem2 then {
+        binaryApproachMedian(nums1, nums2, jumpIdx1, i2, singleMedian, stepsToMedian, stepsTaken+(jumpIdx1-i1))
+      }
+      else {
+        binaryApproachMedian(nums1, nums2, i1, jumpIdx2, singleMedian, stepsToMedian, stepsTaken+(jumpIdx2-i2))
+      }
+    }
+  }
+
+  private def mid(low:Int, high:Int):Int = low + (high-low)/2
+
+  private def median(nums:Array[Int]):Double = {
+    nums.length match
+      case len if len%2 == 1 =>
+        val midC = mid(0,len-1)
+        nums(midC)
+      case len if len%2 == 0 =>
+        val midL = mid(0,len-1)
+        val midR = midL+1
+        (nums(midL)+nums(midR)).toDouble / 2
+  }
+
+  private def medianDisjoint(
+    numsBefore:Array[Int], numsAfter:Array[Int], singleMedian:Boolean, stepsToMedian:Int
+  ):Double = {
+    if stepsToMedian <= numsBefore.length then { //start of median is within nums1
+      if singleMedian then {
+        numsBefore(stepsToMedian-1)
+      }
+      else if stepsToMedian == numsBefore.length then { //two element median spans both arrays
+        (numsBefore.last + numsAfter.head).toDouble / 2
+      }
+      else { //two element median is within nums1
+        (numsBefore(stepsToMedian-1) + numsBefore(stepsToMedian)).toDouble / 2
+      }
+    }
+    else if singleMedian then { //start of median is within nums2
+      numsAfter(stepsToMedian-numsBefore.length-1)
+    }
+    else {
+      val idx = stepsToMedian-numsBefore.length-1
+      (numsAfter(idx) + numsAfter(idx+1)).toDouble / 2
+    }
+  }
+
+  private enum Relation {
+    case Empty
+    case EqualBounds
+    case Contains,Within
+    case DisjointBefore,DisjointAfter
+    case OverlapBefore,OverlapAfter
+  }
+
+  private def classifyRelation(nums1:Array[Int], nums2:Array[Int]):(Relation,Relation) = {
+    (nums1.length, nums2.length) match
+      case (_,0) => (Relation.DisjointBefore, Relation.Empty)
+      case (0,_) => (Relation.Empty, Relation.DisjointAfter)
+      case _     =>
+        (nums1.head, nums1.last, nums2.head, nums2.last) match
+          case (lo1,hi1,lo2,hi2) if hi1<=lo2 => (Relation.DisjointBefore, Relation.DisjointAfter)
+          case (lo1,hi1,lo2,hi2) if hi2<=lo1 => (Relation.DisjointAfter, Relation.DisjointBefore)
+          case (lo1,hi1,lo2,hi2) if lo1==lo2 && hi1==hi2 => (Relation.EqualBounds, Relation.EqualBounds)
+          case (lo1,hi1,lo2,hi2) if lo1<=lo2 && hi1>=hi2 => (Relation.Contains, Relation.Within)
+          case (lo1,hi1,lo2,hi2) if lo1>=lo2 && hi1<=hi2 => (Relation.Within, Relation.Contains)
+          case (lo1,hi1,lo2,hi2) if lo1<=lo2 && hi1<=hi2 => (Relation.OverlapBefore, Relation.OverlapAfter)
+          case (lo1,hi1,lo2,hi2) if lo1>=lo2 && hi1>=hi2 => (Relation.OverlapAfter, Relation.OverlapBefore)
+  }
+
+
 
   def run():FiniteDuration = {
     /* Provided input */
-    val arg1 = Array(1,2)
-    val arg2 = Array(3,4)
+//    val arg1 = Array(1,2)
+//    val arg2 = Array(3,4)
+    val arg1 = Array(1,2,3)
+    val arg2 = Array(4,5,6)
 
     /* Expected output */
-    val exp = 2.5
+//    val exp = 2.5
+    val exp = 3.5
 
     /* Computed output with run time */
     val start = Instant.now()
@@ -50,7 +193,7 @@ object P4_Median_of_Two_Sorted_Arrays extends LeetcodeProblem {
     """.stripMargin)
 
     /* Pass/Fail status */
-    ColorPrinter.printPassFail(pass = false)
+    ColorPrinter.printPassFail(pass = sol==exp)
 
     Duration.between(start,end)
   }
