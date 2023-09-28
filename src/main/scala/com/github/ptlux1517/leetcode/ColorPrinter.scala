09ba17eb3ea6aa2e9b2870aad9394583196f4cd6
Cory Tomlinson <corytomlinson6640@gmail.com>
2023-09-27 19:46:30 -0600

    use scala.Console for built in ANSI color codes

--- src/main/scala/com/github/ptlux1517/leetcode/ColorPrinter.scala
+++ src/main/scala/com/github/ptlux1517/leetcode/ColorPrinter.scala
@@ -1,13 +1,10 @@
 package com.github.ptlux1517.leetcode
 
+import scala.Console.{GREEN,RED,RESET}
+
 
 object ColorPrinter {
-
-   val ANSI_GREEN = "\u001b[32m"
-   val ANSI_RED = "\u001b[31m"
-   val ANSI_RESET = "\u001b[0m"
-
-   def printPassFail(pass:Boolean): Unit =
-     if (pass) println(f"\n${ANSI_GREEN}PASS${ANSI_RESET}")
-     else println(f"\n${ANSI_RED}FAIL${ANSI_RESET}")
+   def printPassFail(pass:Boolean):Unit =
+     if (pass) println(f"\n${GREEN}PASS${RESET}")
+     else println(f"\n${RED}FAIL${RESET}")
 }
